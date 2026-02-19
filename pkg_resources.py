99f5fc9f953029dfc4bdb628f20215322296c6f2
Kirill2397 <golovolomka8794@gmail.com>
2026-02-19 11:54:14 -0800

    Add pkg_resources stub for python 3.14

---
+++ pkg_resources.py
@@ -0,0 +1,15 @@
+"""
+Minimal stub of pkg_resources for compatibility with apscheduler in Python 3.14.
+Provides get_distribution() and DistributionNotFound.
+"""
+class DistributionNotFound(Exception):
+    pass
+
+def get_distribution(name):
+    """
+    Return a dummy distribution object with a version attribute.
+    This is a stub used when pkg_resources is unavailable.
+    """
+    class DummyDist:
+        version = "0.0"
+    return DummyDist()
